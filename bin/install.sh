8b481132b933b2bf989c29885671a399dc88a12f
Jakub Roman <jakub@jakubroman.sk>
2023-11-27 14:04:28 +0100

    error handling

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -eo pipefail
 
 log_info "Installing/Updating direnv..."
 
-curl -s https://direnv.net/install.sh | bash
+curl -s https://direnv.net/install.sh | bash || exit 1
 
 log_info "You can safely ignore any instructions printed above"
